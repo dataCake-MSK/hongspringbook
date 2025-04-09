1667624ae790fa818fe571e3cb9e0cd073d5f73f
MinsungKang <minsungkang2012@gmail.com>
2025-04-09 14:14:07 +0900

    added entity class(Article) file with fields, JPA supported annotations, and important methods(constructor, toString)

---
+++ src/main/java/com/min/hongspringbook/entity/Article.java
@@ -0,0 +1,29 @@
+package com.min.hongspringbook.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
