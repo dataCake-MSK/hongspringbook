f5d70953bd5ae5afb0b754c5979b26b460d40e82
Minsung Kang <minsungkang2012@gmail.com>
2025-06-08 17:37:01 +0900

    1. Added code, in ApplicationRunner, to create sample data

--- src/main/java/com/min/hongspringbook/runner/Runner.java
+++ src/main/java/com/min/hongspringbook/runner/Runner.java
@@ -1,16 +1,33 @@
 package com.min.hongspringbook.runner;
 
-import org.hibernate.annotations.Comment;
+import com.min.hongspringbook.dto.ArticleForm;
+import com.min.hongspringbook.entity.Article;
+import com.min.hongspringbook.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class Runner implements ApplicationRunner {
+
+    private final ArticleRepository articleRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("ApplicationRunner ran with non-option args: " + args.getNonOptionArgs());
-        System.out.println("and with args: " + args.getOptionNames() + args.getOptionValues("mykey"));
+        ArticleForm form = new ArticleForm(null, "TEST TITLE1", "TEST CONTENT1");
+        Article article = form.toEntity();
+        Article saved = articleRepository.save(article);
+        System.out.println("Saved dummy data: " + saved);
+
+        form = new ArticleForm(null, "TEST TITLE2", "TEST CONTENT2");
+        article = form.toEntity();
+        saved = articleRepository.save(article);
+        System.out.println("Saved dummy data: " + saved);
+//        System.out.println("ApplicationRunner ran with non-option args: " + args.getNonOptionArgs());
+//        System.out.println("and with args: " + args.getOptionNames() + args.getOptionValues("mykey"));
     }
+
 }
 
