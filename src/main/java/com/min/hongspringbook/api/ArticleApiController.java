d09be8831a41cf1bc56e35654041dd93776c7d1a
Minsung Kang <minsungkang2012@gmail.com>
2025-06-08 15:48:24 +0900

    1. Added PATCH /api/articles/{id} endpoint. 2. Added exception logic, 3. Made methods return ResponseEntity, 4. Added id field to DTO and replaced null.

--- src/main/java/com/min/hongspringbook/api/ArticleApiController.java
+++ src/main/java/com/min/hongspringbook/api/ArticleApiController.java
@@ -1,20 +1,45 @@
 package com.min.hongspringbook.api;
 
+import com.min.hongspringbook.dto.ArticleForm;
 import com.min.hongspringbook.entity.Article;
 import com.min.hongspringbook.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ArticleApiController {
     private final ArticleRepository articleRepository;
 
     @GetMapping("/api/articles")
-    public List<Article> index(){
-        return (List<Article>) articleRepository.findAll();
+    public ResponseEntity<?> index(){
+        return ResponseEntity.status(HttpStatus.OK).body(articleRepository.findAll());
+    }
+
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ArticleForm dto){
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        if (!Objects.equals(id, article.getId())){
+            log.info(String.valueOf(article.getId()));
+            log.info("Body and URL id mismatch!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Body and URL id mismatch!");
+        }
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null){
+            String message = String.format("Id %d Entity doesn't exist", id);
+            log.info(message);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+        }
+        Article updated = articleRepository.save(article);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 }
